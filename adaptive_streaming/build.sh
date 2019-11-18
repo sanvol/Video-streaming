ba296aeb786945e590990be48fe02053ee0aa674
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2019-11-18 18:25:38 +0200

    Add video files to container manually

--- adaptive_streaming/build.sh
+++ adaptive_streaming/build.sh
@@ -13,8 +13,12 @@ confirm() {
     esac
 }
 
+container() {
+    echo $(docker ps | grep $1 | awk {'print $1'})
+}
+
 remove_container() {
-    CONTAINER=$(docker ps | grep $1 | awk {'print $1'})
+    CONTAINER=$(container $1)
     docker stop $CONTAINER
     docker rm $CONTAINER
 }
@@ -24,11 +28,9 @@ cd $(dirname $0)
 # CONFIGS
 IMAGE=adaptive_streaming
 NAME=nginx-rtmp
-DASH_VOLUME=dash_volume
-HLS_VOLUME=hls_volume
+# STREAMING_VOLUME=dash_volume
 
-docker volume create $DASH_VOLUME > /dev/null
-docker volume create $HLS_VOLUME > /dev/null
+# docker volume create $STREAMING_VOLUME > /dev/null
 
 while true; do
     # For static content:
@@ -36,11 +38,14 @@ while true; do
     # --mount source=$CONTENT,target=/usr/local/nginx/html/
     if docker build -t $IMAGE . && \
         docker run -p 8080:8080 -p 1935:1935 \
-        --mount source=$DASH_VOLUME,target=/var/lib/dash \
-        --mount source=$HLS_VOLUME,target=/var/lib/hls \
         -d $IMAGE ; then
         echo "Container running"
-        break;  
+        if confirm "Do you want to copy files from dashvideo/ to container? [y/n]" ; then
+            c=$(container $IMAGE)
+            echo "Copying videos to container ${c}"
+            docker cp dashvideo/. $c:/var/lib/stream/dash/
+        fi
+        break;
     elif confirm "Would you like to remove old image? [y/n]" ; then
         remove_container $IMAGE >> /dev/null
         continue;
